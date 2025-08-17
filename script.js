062d3ad2f64b3d4b11bd844e7e0e28f00c35b316
arvind52yadav <arvind52yadav@gmail.com>
2025-08-17 10:26:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 const animals = [
-    { name: "Dog", image: "dog.jpg" },
-    { name: "Cat", image: "cat.jpg" },
-    { name: "Lion", image: "lion.jpg" },
-    { name: "Elephant", image: "elephant.jpg" },
+    { name: "Dog", image: "https://googleusercontent.com/image_collection/image_retrieval/3698286948941955453_0" },
+    { name: "Cat", image: "https://googleusercontent.com/image_collection/image_retrieval/3469785087538742540_0" },
+    { name: "Lion", image: "https://googleusercontent.com/image_collection/image_retrieval/10313061872124986872_0" },
+    { name: "Elephant", image: "https://googleusercontent.com/image_collection/image_retrieval/18396195170509000977_0" },
+    { name: "Tiger", image: "https://googleusercontent.com/image_collection/image_retrieval/11507776071823835758_0" },
+    { name: "Bear", image: "https://googleusercontent.com/image_collection/image_retrieval/2569584732104523955_0" },
+    { name: "Zebra", image: "https://googleusercontent.com/image_collection/image_retrieval/12023023247954930263_0" },
+    { name: "Duck", image: "https://googleusercontent.com/image_collection/image_retrieval/1031737728514571066_0" }
 ];
 
 const fruits = [
-    { name: "Apple", image: "apple.jpg" },
-    { name: "Banana", image: "banana.jpg" },
-    { name: "Orange", image: "orange.jpg" },
-    { name: "Strawberry", image: "strawberry.jpg" },
+    { name: "Apple", image: "https://googleusercontent.com/image_collection/image_retrieval/16518694356068693616_0" },
+    { name: "Banana", image: "https://googleusercontent.com/image_collection/image_retrieval/2252587650617642401_0" },
+    { name: "Orange", image: "https://googleusercontent.com/image_collection/image_retrieval/2879543419651495932_0" },
+    { name: "Strawberry", image: "https://googleusercontent.com/image_collection/image_retrieval/4891281485437273781_0" },
+    { name: "Grapes", image: "https://googleusercontent.com/image_collection/image_retrieval/12401337695873111206_0" },
+    { name: "Mango", image: "https://googleusercontent.com/image_collection/image_retrieval/3909774676176527506_0" },
+    { name: "Kiwi", image: "https://googleusercontent.com/image_collection/image_retrieval/13812239459523277028_0" },
+    { name: "Pineapple", image: "https://googleusercontent.com/image_collection/image_retrieval/6312151664183617154_0" }
 ];
 
 let currentCategory = [];
@@ -26,10 +34,10 @@ const categoryTiles = document.querySelector(".category-tiles");
 
 function displayFlashcard() {
     if (currentCategory.length === 0) {
-        return; // Don't do anything if no category is selected
+        return; 
     }
     const item = currentCategory[currentIndex];
-    cardImage.src = `images/${item.image}`;
+    cardImage.src = item.image;
     cardName.textContent = item.name;
 }
 
@@ -51,7 +59,6 @@ function handlePrev() {
     displayFlashcard();
 }
 
-// Event Listeners for the tiles
 animalsTile.addEventListener("click", () => {
     showCategory(animals);
 });
@@ -60,6 +67,5 @@ fruitsTile.addEventListener("click", () => {
     showCategory(fruits);
 });
 
-// Event listeners for the flashcard buttons
 nextBtn.addEventListener("click", handleNext);
 prevBtn.addEventListener("click", handlePrev);
